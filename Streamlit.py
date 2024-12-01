e1c4a8727ecbd6a5ff767077a768a85fd04814fd
Danica26 <danicadagohoy21@gmail.com>
2024-12-01 20:20:47 +0800

    Update Streamlit.py
    Signed-off-by: Danica26 <danicadagohoy21@gmail.com>

--- Streamlit.py
+++ Streamlit.py
@@ -71,7 +71,9 @@ with col1:
     # Editable Hobbies/Interests
     st.header("Hobbies & Interests")
     hobbies = st.text_area("List your hobbies or interests", 
-                           "- Playing Badminton & Volleybal, -Swimming, -Photography")
+                           "- Playing Badminton & Volleybal 
+                           - Swimming 
+                           - Photography")
 # Editable Photo (in the second column)
 with col2:
     st.subheader("Photo")
